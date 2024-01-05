13f765c1f9c81e5a0285a4c80ee71725b15413d9
Joaquín <joaquingomezleites@gmail.com>
2024-01-05 10:14:25 -0300

    checkout form progress
    TODO: MercadoPago integration

--- app/(dashboard)/[storeId]/(routes)/ordenes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/ordenes/page.tsx
@@ -30,7 +30,7 @@ const OrdersPage = async ({
     const formattedOrders: OrderColumn[] = orders.map((item) => ({
         id: item.id,
         phone: item.phone,
-        address: item.address,
+        address: "placeholder",
         products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
         totalPrice: formatter.format(item.orderItems.reduce((total, item) => {
             return total + Number(item.product.price)
